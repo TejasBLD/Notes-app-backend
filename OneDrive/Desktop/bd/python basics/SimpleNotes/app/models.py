0b09655d6845382587f322045c4d4b1b7c23d285
TejasBLD <worktejas2033@gmail.com>
2026-01-11 19:59:55 +0530

    Finalize FastAPI notes backend

---
+++ OneDrive/Desktop/bd/python basics/SimpleNotes/app/models.py
@@ -0,0 +1,23 @@
+from .database import Base
+from sqlalchemy import Column,String,Text,DateTime
+from datetime import datetime
+import uuid
+class Note(Base):
+    __tablename__="notes"
+    id=Column(
+        String,
+        primary_key=True,
+        default=lambda:str(uuid.uuid4())
+    )
+    title=Column(
+        String,
+        nullable=False
+    )
+    content=Column(
+        Text,
+        nullable=False
+    )
+    date_of_creation=Column(
+        DateTime,
+        datetime,default=datetime.utcnow
+    )
